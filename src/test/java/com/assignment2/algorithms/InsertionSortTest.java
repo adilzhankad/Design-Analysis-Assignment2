7be3468440789525e9c8c5232f79d7bfa0f412c8
adilzhankadyrov <akomen1000@gmail.con>
2025-10-04 23:29:29 +0500

    test(algorithm): add reverse sorted and duplicate array tests

--- src/test/java/com/assignment2/algorithms/InsertionSortTest.java
+++ src/test/java/com/assignment2/algorithms/InsertionSortTest.java
@@ -2,15 +2,26 @@ package com.assignment2.algorithms;
 
 import com.assignment2.metrics.PerformanceTracker;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class InsertionSortTest {
 
     @Test
-    public void testSortBasic() {
-        int[] arr = {5, 6, 2, 3, 1};
-        int[] expected = {1, 2, 3, 5, 6};
+    public void testReverseSorted() {
+        int[] arr = {5, 4, 3, 2, 1};
+        int[] expected = {1, 2, 3, 4, 5};
+
+        PerformanceTracker tracker = new PerformanceTracker();
+        InsertionSort sorter = new InsertionSort(tracker);
+        sorter.sort(arr);
+
+        assertArrayEquals(expected, arr);
+    }
+
+    @Test
+    public void testDuplicates() {
+        int[] arr = {3, 1, 2, 3, 1};
+        int[] expected = {1, 1, 2, 3, 3};
 
         PerformanceTracker tracker = new PerformanceTracker();
         InsertionSort sorter = new InsertionSort(tracker);
